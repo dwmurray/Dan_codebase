0a8995f9c120127871460b379cc16411a8444aca
Daniel <dwmurray@asu.edu>
2019-05-17 16:59:36 -0500

    minor updates

--- ramses_dan/patch/amr/dan/amr_parameters.f90
+++ ramses_dan/patch/amr/dan/amr_parameters.f90
@@ -180,8 +180,8 @@ module amr_parameters
   ! Output times
   real(dp),dimension(1:MAXOUT)::aout=1.1       ! Output expansion factors
   real(dp),dimension(1:MAXOUT)::tout=0.0       ! Output times
-  real(dp)::delta_tout=0,tend=0 !DWM 2019
-  real(dp)::delta_aout=0,aend=0 !DWM 2019
+!  real(dp)::delta_tout=0,tend=0 !DWM 2019
+!  real(dp)::delta_aout=0,aend=0 !DWM 2019
 
   ! Movie
   integer,parameter::NMOV=5
