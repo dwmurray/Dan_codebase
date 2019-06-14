600379ebe9c1315e0fe346e99f1cc56b484c30d2
Daniel <dwmurray@asu.edu>
2019-06-14 10:57:13 -0600

    added write statements to Phil's version of ramses, looking to compare to Ricks

--- ramses_phil/hydro/godunov_utils.f90
+++ ramses_phil/hydro/godunov_utils.f90
@@ -18,9 +18,11 @@ subroutine cmpdt(uu,gg,dx,dt,ncell)
   smallp = smallc**2/gamma
 
   ! Convert to primitive variables
+  write(*,*) 'Inside subroutine cmpdt, uu(ncell/2,1): ', uu(ncell/2.,1)
   do k = 1,ncell
      uu(k,1)=max(uu(k,1),smallr)
   end do
+  write(*,*) 'subroutine cmpdt max between uu and smallr, uu(ncell/2,1): ', uu(ncell/2.,1)
   ! Velocity
   do idim = 1,ndim
      do k = 1, ncell
@@ -94,6 +96,7 @@ subroutine cmpdt(uu,gg,dx,dt,ncell)
   end do
 
   ! Compute gravity strength ratio
+  write(*,*) 'about to zero uu for gravity-subroutine cmpdt, uu(ncell/2,1): ', uu(ncell/2.,1)
   do k = 1, ncell
      uu(k,1)=zero
   end do
