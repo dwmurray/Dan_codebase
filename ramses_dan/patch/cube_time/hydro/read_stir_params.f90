0e0a8e1ecb0519f740f496eeca1457f53542db5d
Daniel Murray <dwmurray@asu.edu>
2019-09-30 19:01:46 -0500

    modified our read in to allow user to set a specified deltaT over which the stir accel field is modified

--- ramses_dan/patch/cube_time/hydro/read_stir_params.f90
+++ ramses_dan/patch/cube_time/hydro/read_stir_params.f90
@@ -2,11 +2,12 @@ subroutine read_stir_params(nml_ok)
   use stir_parameters
   implicit none
   logical::nml_ok
+  integer:: i !DWM setup stir timescale
   !--------------------------------------------------
   ! Namelist definitions
   !--------------------------------------------------
   namelist/stir_params/stir_seed,stir_norm,stir_index,& 
-        stir_kmin,stir_kmax
+        stir_kmin,stir_kmax,stir_delta_t
   rewind(1)
   read(1,NML=stir_params,END=101)
   goto 102
@@ -15,5 +16,12 @@ subroutine read_stir_params(nml_ok)
   call clean_stop
 102 rewind(1)
   nml_ok=.true.
+
+  ! ------------ DWM
+  ! COmpute time step to modify the stirring acc field.
+  !---------------
+  do i=1,noutput
+     stir_timescale(i)=dble(i)*stir_delta_t
+  end do
 end subroutine read_stir_params
 
